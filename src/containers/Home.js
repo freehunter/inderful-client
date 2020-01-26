f2a20f3886b2961205e3e3f2157b737f504ecfe3
Mark Hunt <thefreehunter@gmail.com>
2020-01-26 23:47:15 +0000

    Setting up our React app

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Inderful</h1>
+        <p>Self care for introverts</p>
+      </div>
+    </div>
+  );
+}
