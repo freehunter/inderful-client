ea4b7ee07bb748f05ac6a17f1e41cb8f157b4d7e
Mark Hunt <thefreehunter@gmail.com>
2020-01-27 03:07:25 +0000

    adding front end for the apis

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import AppliedRoute from "./components/AppliedRoute";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
-  s3: {
-    REGION: "us-west-2",
-    BUCKET: "inderful"
-  },
-  apiGateway: {
-    REGION: "us-west-2",
-    URL: "https://wy25mn1ppc.execute-api.us-west-2.amazonaws.com/prod"
-  },
-  cognito: {
-    REGION: "us-west-2",
-    USER_POOL_ID: "us-west-2_qOwei7eCL",
-    APP_CLIENT_ID: "1epuvi75bq448ljaoc56lm8oab",
-    IDENTITY_POOL_ID: "us-west-2:328e7d88-3e9f-4d28-862d-a6c6da037581"
-  }
+    MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "inderful"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://wy25mn1ppc.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_qOwei7eCL",
+      APP_CLIENT_ID: "1epuvi75bq448ljaoc56lm8oab",
+      IDENTITY_POOL_ID: "us-west-2:328e7d88-3e9f-4d28-862d-a6c6da037581"
+    }
 };
